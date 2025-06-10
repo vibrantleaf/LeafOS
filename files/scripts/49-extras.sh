5bdd4ad8af6750aecfdf84ebc3a61ff5840df725
Julia <sea.leaf@outlook.com>
2025-06-10 15:46:55 +1000

    Update 49-extras.sh

--- files/scripts/49-extras.sh
+++ files/scripts/49-extras.sh
@@ -30,6 +30,8 @@ install -Dm 644 /opt/org.codeberg.vibrantleaf.brew-gui/brew-gui.sh /usr/bin/brew
 install -Dm 644 /opt/org.codeberg.vibrantleaf.brew-gui/README.en_UK.md /usr/share/org.codeberg.vibrantleaf.brew-gui/README.en_UK.md
 install -Dm 644 /opt/org.codeberg.vibrantleaf.brew-gui/LICENSE.en_UK.txt /usr/share/org.codeberg.vibrantleaf.brew-gui/LICENSE.en_UK.txt
 install -Dm 644 /opt/org.codeberg.vibrantleaf.brew-gui/assets/org.codeberg.vibrantleaf.brew-gui.svg /usr/share/icons/hicolor/scalable/apps/org.codeberg.vibrantleaf.brew-gui.svg
+install -Dm 644 /opt/org.codeberg.vibrantleaf.brew-gui/assets/org.codeberg.vibrantleaf.brew-gui.svg /usr/share/icons/hicolor/256x256/apps/org.codeberg.vibrantleaf.brew-gui.png
+
 chmod +x /usr/bin/brew-gui
 
 
