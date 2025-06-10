7f72626dc645aa8d8bd2519f9d231da038c33577
Julia <sea.leaf@outlook.com>
2025-06-10 14:50:45 +1000

    use the bluebuild cli installer script rather than copying the cli from the container

--- files/scripts/50-cleanup.sh
+++ files/scripts/50-cleanup.sh
@@ -4,6 +4,7 @@ rm -rf /opt/org.codeberg.vibrantleaf.vibrant-updater
 rm -rf /opt/io.looking-glass.client
 rm -rf /opt/com.github.vibrantleaf.sharkfin/
 rm -rf /opt/org.codeberg.vibrantleaf.brew-gui
+rm -rf /opt/org.bluebuild.cli/
 rm -rf /opt/com.github.gist.AggamR.get_ms_fonts_from_a_windows_10_iso
 rm -rf /usr/share/gnome-shell/extensions/tilingshell@ferrarodomenico.com
 rm -rf /usr/share/sounds/steam
