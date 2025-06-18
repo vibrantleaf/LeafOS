62acdc86deb71f87a6893befc285d37f8a5c3c1b
Julia <sea.leaf@outlook.com>
2025-06-19 08:23:44 +1000

    Update 49-extras.sh

--- files/scripts/49-extras.sh
+++ files/scripts/49-extras.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
-#rm -fv /etc/dnf/protected.d/sudo.conf"
-
 git clone https://github.com/vibrantleaf/sharkfin.git /opt/com.github.vibrantleaf.sharkfin
 mkdir -p /usr/share/sharkfin
-
 cp -fv /opt/com.github.vibrantleaf.sharkfin/cosign.pub /usr/share/sharkfin/cosign.pub
-
-#sed -i 's@Exec=/usr/bin/bazzite-steam@Exec=/usr/bin/steam@g' /usr/share/applications/steam.desktop
-#sed -i 's@Exec=/usr/bin/bazzite-steam-bpm@Exec=/usr/bin/steam -gamepadui@g' /usr/share/applications/steam.desktop
-#sed -i 's@Exec=/usr/bin/bazzite-steam-bpm@Exec=/usr/bin/steam -gamepadui@g' /usr/share/applications/bazzite-steam-bpm.desktop
-
 git clone https://gist.github.com/AggamR/314eaa3ef6d5488ed7ebd9eb39404390.git /opt/com.github.gist.AggamR.get_ms_fonts_from_a_windows_10_iso
 install -Dm 644 /opt/com.github.gist.AggamR.get_ms_fonts_from_a_windows_10_iso/getMsFontsIso.sh /usr/libexec/extract_fonts_from_windows_iso
 chmod +x /usr/libexec/extract_fonts_from_windows_iso
@@ -18,11 +10,14 @@ chmod +x /usr/libexec/extract_fonts_from_windows_iso
 chmod 644 /usr/share/applications/stop-waydroid.desktop
 chmod 644 /usr/share/applications/reset-waydroid.desktop
 chmod 644 /usr/share/applications/toggle-automatic-system-updates.desktop
+chmod 644 /usr/share/applications/steam-big-picture-mode.desktop
+chmod 644 /usr/share/applications/brew-gui.desktop
+chmod 644 /usr/share/applications/looking-glass.desktop
+chmod 644 /usr/share/applications/vibrant-updater.desktop
 
 bluebuild completions bash | tee /usr/share/bash-completion/completions/bluebuild
 chmod 644 /usr/bin/bluebuild
 chmod +x /usr/bin/bluebuild
-
 
 git clone https://codeberg.org/vibrantleaf/brew-gui.git /opt/org.codeberg.vibrantleaf.brew-gui
 mkdir -p /usr/share/org.codeberg.vibrantleaf.brew-gui/
@@ -33,7 +28,6 @@ install -Dm 644 /opt/org.codeberg.vibrantleaf.brew-gui/assets/org.codeberg.vibra
 install -Dm 644 /opt/org.codeberg.vibrantleaf.brew-gui/assets/org.codeberg.vibrantleaf.brew-gui.svg /usr/share/icons/hicolor/256x256/apps/org.codeberg.vibrantleaf.brew-gui.png
 
 chmod +x /usr/bin/brew-gui
-
 chmod +x /usr/libexec/sharkfin-waydroid-utils-reset
 chmod +x /usr/libexec/sharkfin-waydroid-utils-stop
 
