3ac351c020a12bb52b6451efae63152543c1d9c3
Julia Rees <sea.leaf@outlook.com>
2025-06-27 23:19:29 +1000

    Update 41-get-looking-glass.sh

--- files/scripts/41-get-looking-glass.sh
+++ files/scripts/41-get-looking-glass.sh
@@ -2,7 +2,9 @@
 
 git clone --recursive https://github.com/gnif/LookingGlass.git /opt/io.looking-glass.client
 
-(cd /opt/io.looking-glass.client/;git fetch --tags && git checkout $(git describe --tags '$(git rev-list --tags --max-count=1)'))
+(cd /opt/io.looking-glass.client/;git fetch --tags)
+(cd /opt/io.looking-glass.client/;latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+(cd /opt/io.looking-glass.client/;git checkout $latestTag )
 
 mkdir -p /opt/io.looking-glass.client/client/build
 (cd /opt/io.looking-glass.client/client/build;cmake -DENABLE_PIPEWIRE=no -DENABLE_LIBDECOR=ON -DCMAKE_INSTALL_PREFIX=/usr ../)
