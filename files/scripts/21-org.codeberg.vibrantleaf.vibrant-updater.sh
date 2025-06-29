8b73cf7fafc068f1760b76640c25e3d8f9692b71
Julia Rees <sea.leaf@outlook.com>
2025-06-30 06:38:29 +1000

    use files from repo

--- files/scripts/21-org.codeberg.vibrantleaf.vibrant-updater.sh
+++ files/scripts/21-org.codeberg.vibrantleaf.vibrant-updater.sh
@@ -9,12 +9,18 @@ git clone https://codeberg.org/vibrantleaf/vibrant-updater.git /sources/org.code
 (cd /sources/org.codeberg.vibrantleaf.vibrant-updater/;latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)"))
 (cd /sources/org.codeberg.vibrantleaf.vibrant-updater/;git checkout $latestTag)
 (cd /sources/org.codeberg.vibrantleaf.vibrant-updater/;go build -o /usr/bin/vibrant-updater /sources/org.codeberg.vibrantleaf.vibrant-updater/src/vibrant_updater.go)
-mkdir -p /usr/share/vibrant-updater/
-install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/LICENSE.txt /usr/share/vibrant-updater/LICENSE.en.txt
-install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/src/VibrantUpdater.just /usr/share/vibrant-updater/VibrantUpdater.just
-install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/src/VibrantUpdater-user.timer /usr/share/vibrant-updater/VibrantUpdater-user.timer
+mkdir -p /usr/share/org.codeberg.vibrantleaf.vibrant-updater/
+mkdir -p /usr/share/licenses/org.codeberg.vibrantleaf.vibrant-updater/
+install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/bin/exec-vibrant-updater.sh /usr/libexec/exec-vibrant-updater
+install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/LICENSE.en_UK.txt /usr/share/licenses/org.codeberg.vibrantleaf.vibrant-updater/LICENSE.en_UK.txt
+install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/README.en_UK.md /usr/share/org.codeberg.vibrantleaf.vibrant-updater/README.en_UK.md
+install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/src/org.codeberg.vibrantleaf.vibrant-updater.desktop /usr/share/applications/org.codeberg.vibrantleaf.vibrant-updater.desktop
+install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/src/org-codeberg.vibrantleaf-vibrant-updater-sys.service /etc/systemd/system/org-codeberg.vibrantleaf-vibrant-updater-sys.service
+install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/src/org-codeberg.vibrantleaf-vibrant-updater-sys.timer /etc/systemd/system/org-codeberg-vibrantleaf-vibrant-updater-sys.timer
+install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/src/org-codeberg.vibrantleaf-vibrant-updater-user.service /usr/share/org.codeberg.vibrantleaf.vibrant-updater/org-codeberg-vibrantleaf-vibrant-updater-user.service
+install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/src/org-codeberg.vibrantleaf-vibrant-updater-user.timer /usr/share/org.codeberg.vibrantleaf.vibrant-updater/org-codeberg-vibrantleaf-vibrant-updater-user.timer
+install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/src/org.codeberg.vibrantleaf.vibrant-updater.just /usr/share/org.codeberg.vibrantleaf.vibrant-updater/org.codeberg.vibrantleaf.vibrant-updater.just
 install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/assets/Logo_Circle.svg /usr/share/icons/hicolor/scalable/apps/org.codeberg.vibrantleaf.vibrant-updater.svg
-chmod 644 /usr/share/applications/org.codeberg.vibrantleaf.vibrant-updater.desktop
-chmod 644 /usr/libexec/exec-vibrant-updater
+install -Dm 644 /sources/org.codeberg.vibrantleaf.vibrant-updater/assets/Logo_Circle.png /usr/share/icons/hicolor/256x256/apps/org.codeberg.vibrantleaf.vibrant-updater.png
 chmod +x /usr/libexec/exec-vibrant-updater
 chmod +x /usr/bin/vibrant-updater
