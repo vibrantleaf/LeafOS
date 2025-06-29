90b528ca6034e763bcb1076cb997ae972f59024e
Julia Rees <sea.leaf@outlook.com>
2025-06-30 06:41:56 +1000

    Update 20-org.codeberg.vibrantleaf.brew-gui.sh

--- files/scripts/20-org.codeberg.vibrantleaf.brew-gui.sh
+++ files/scripts/20-org.codeberg.vibrantleaf.brew-gui.sh
@@ -6,9 +6,10 @@ set -o pipefail
 mkdir -p /sources
 git clone https://codeberg.org/vibrantleaf/brew-gui.git /sources/org.codeberg.vibrantleaf.brew-gui
 mkdir -p /usr/share/org.codeberg.vibrantleaf.brew-gui/
+mkdir -p /usr/share/licenses/org.codeberg.vibrantleaf.brew-gui/
 install -Dm 644 /sources/org.codeberg.vibrantleaf.brew-gui/src/brew-gui.sh /usr/bin/brew-gui
 install -Dm 644 /sources/org.codeberg.vibrantleaf.brew-gui/README.en_UK.md /usr/share/org.codeberg.vibrantleaf.brew-gui/README.en_UK.md
-install -Dm 644 /sources/org.codeberg.vibrantleaf.brew-gui/LICENSE.en_UK.txt /usr/share/org.codeberg.vibrantleaf.brew-gui/LICENSE.en_UK.txt
+install -Dm 644 /sources/org.codeberg.vibrantleaf.brew-gui/LICENSE.en_UK.txt /usr/share/licenses/org.codeberg.vibrantleaf.brew-gui/LICENSE.en_UK.txt
 install -Dm 644 /sources/org.codeberg.vibrantleaf.brew-gui/src/org.codeberg.vibrantleaf.brew-gui.desktop /usr/share/applications/org.codeberg.vibrantleaf.brew-gui.desktop
 install -Dm 644 /sources/org.codeberg.vibrantleaf.brew-gui/assets/org.codeberg.vibrantleaf.brew-gui.svg /usr/share/icons/hicolor/scalable/apps/org.codeberg.vibrantleaf.brew-gui.svg
 install -Dm 644 /sources/org.codeberg.vibrantleaf.brew-gui/assets/org.codeberg.vibrantleaf.brew-gui.png /usr/share/icons/hicolor/256x256/apps/org.codeberg.vibrantleaf.brew-gui.png
