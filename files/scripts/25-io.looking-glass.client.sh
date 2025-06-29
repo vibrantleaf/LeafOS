4d6a0adcda3a695abcb3f196f78e4830c57a6067
Julia Rees <sea.leaf@outlook.com>
2025-06-30 06:47:58 +1000

    Update 25-io.looking-glass.client.sh

--- files/scripts/25-io.looking-glass.client.sh
+++ files/scripts/25-io.looking-glass.client.sh
@@ -11,12 +11,13 @@ git clone --recursive https://github.com/gnif/LookingGlass.git /sources/io.looki
 mkdir -p /sources/io.looking-glass.client/client/build
 (cd /sources/io.looking-glass.client/client/build;cmake -DENABLE_PIPEWIRE=no -DENABLE_LIBDECOR=ON -DCMAKE_INSTALL_PREFIX=/usr ../)
 (cd /sources/io.looking-glass.client/client/build;make)
-mkdir -p /usr/share/doc/looking-glass-client/
+mkdir -p /usr/share/io.looking-glass.client/
+mkdir -p /usr/share/licenses/io.looking-glass.client
 install -Dm 644 /sources/io.looking-glass.client/client/build/looking-glass-client /usr/bin/looking-glass-client
-install -Dm 644 /sources/io.looking-glass.client/README.md /usr/share/doc/looking-glass-client/README.md
-install -Dm 644 /sources/io.looking-glass.client/LICENSE /usr/share/doc/looking-glass-client/LICENSE
-install -Dm 644 /sources/io.looking-glass.client/AUTHORS /usr/share/doc/looking-glass-client/AUTHORS
-install -Dm 644 /sources/io.looking-glass.client/CONTRIBUTORS /usr/share/doc/looking-glass-client/CONTRIBUTORS
+install -Dm 644 /sources/io.looking-glass.client/README.md /usr/share/looking-glass-client/README.md
+install -Dm 644 /sources/io.looking-glass.client/LICENSE /usr/share/licenses/io.looking-glass.client/LICENSE.txt
+install -Dm 644 /sources/io.looking-glass.client/AUTHORS /usr/share/licenses/looking-glass-client/AUTHORS
+install -Dm 644 /sources/io.looking-glass.client/CONTRIBUTORS /usr/share/licenses/looking-glass-client/CONTRIBUTORS
 install -Dm 644 /sources/io.looking-glass.client/resources/lg-logo.svg /usr/share/icons/hicolor/scalable/apps/io.looking-glass.client.svg
 chmod 644 /usr/share/applications/io.looking-glass.client.desktop
 chmod +x /usr/bin/looking-glass-client
