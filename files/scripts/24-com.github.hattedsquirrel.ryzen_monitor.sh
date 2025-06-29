44033a6ebeff465d8a70f64687ace1104ed8352b
Julia Rees <sea.leaf@outlook.com>
2025-06-30 06:45:43 +1000

    Update 24-com.github.hattedsquirrel.ryzen_monitor.sh

--- files/scripts/24-com.github.hattedsquirrel.ryzen_monitor.sh
+++ files/scripts/24-com.github.hattedsquirrel.ryzen_monitor.sh
@@ -4,10 +4,11 @@ set -e
 set -u
 set -o pipefail
 mkdir -p /sources
-git clone git clone https://github.com/hattedsquirrel/ryzen_monitor.git /sources/com.github.hattedsquirrel.ryzen_monitor
+git clone https://github.com/hattedsquirrel/ryzen_monitor.git /sources/com.github.hattedsquirrel.ryzen_monitor
 (cd /sources/com.github.hattedsquirrel.ryzen_monitor;make)
 mkdir -p /usr/share/com.github.hattedsquirrel.ryzen_monitor
+mkdir -p /usr/share/licenses/com.github.hattedsquirrel.ryzen_monitor/
 install -Dm 644 /sources/com.github.hattedsquirrel.ryzen_monitor/src/ryzen_monitor /usr/bin/ryzen-monitor
-install -Dm 644 /sources/com.github.hattedsquirrel.ryzen_monitor/LICENSE /usr/share/com.github.hattedsquirrel.ryzen_monitor/LICENSE.txt
+install -Dm 644 /sources/com.github.hattedsquirrel.ryzen_monitor/LICENSE /usr/share/licenses/com.github.hattedsquirrel.ryzen_monitor/LICENSE.txt
 install -Dm 644 /sources/com.github.hattedsquirrel.ryzen_monitor/README.md /usr/share/com.github.hattedsquirrel.ryzen_monitor/README.md
 chmod +x /usr/bin/ryzen-monitor
