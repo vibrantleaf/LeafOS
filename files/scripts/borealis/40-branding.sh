103571c48169490384663bcc5f04ce2b9d985417
Julia Rees <sea.leaf@outlook.com>
2025-07-01 18:34:46 +1000

    Update 40-branding.sh

--- files/scripts/borealis/40-branding.sh
+++ files/scripts/borealis/40-branding.sh
@@ -3,11 +3,12 @@ set -x
 set -e
 set -u
 set -o pipefail
-sed -i 's@bluefin@sharkfin@g' /usr/share/ublue-os/image-info.json
+sed -i 's@bluefin@borealis@g' /usr/share/ublue-os/image-info.json
 sed -i 's@ublue-os@vibrantleaf@g' /usr/share/ublue-os/image-info.json
-sed -i 's@Bluefin@borealis@g' /usr/lib/os-release
-sed -i 's@bluefin@borealis@g' /usr/lib/os-release
 sed -i 's@https://projectbluefin.io@https://github.com/vibrantleaf/sharkfin@g' /usr/lib/os-release
 sed -i 's@https://docs.projectbluefin.io@https://github.com/vibrantleaf/sharkfin/wiki@g' /usr/lib/os-release
 sed -i 's@https://discord.b@https://github.com/vibrantleaf/sharkfin/discussions@g' /usr/lib/os-release
 sed -i 's@https://github.com/ublue-os/bluefin/issues/@https://github.com/vibrantleaf/sharkfin/issues@g' /usr/lib/os-release
+sed -i 's@Aurora@borealis@g' /usr/lib/os-release
+sed -i 's@aurora@borealis@g' /usr/lib/os-release
+sed -i 's@universal-blue@vibrantleaf@g' /usr/lib/os-release
