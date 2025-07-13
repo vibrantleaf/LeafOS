9a9d5c02fbadc92d776a178202d9584f540b954a
Julia Rees <sea.leaf@outlook.com>
2025-07-13 19:56:33 +0800

    brand sharkbox & shetland

--- files/scripts/sharkfin/40-branding.sh
+++ files/scripts/sharkfin/40-branding.sh
@@ -5,10 +5,10 @@ set -u
 set -o pipefail
 sed -i 's@bluefin@sharkfin@g' /usr/share/ublue-os/image-info.json
 sed -i 's@ublue-os@vibrantleaf@g' /usr/share/ublue-os/image-info.json
-sed -i 's@https://projectbluefin.io@https://github.com/vibrantleaf/sharkfin@g' /usr/lib/os-release
-sed -i 's@https://docs.projectbluefin.io@https://github.com/vibrantleaf/sharkfin/wiki@g' /usr/lib/os-release
-sed -i 's@https://discord.b@https://github.com/vibrantleaf/sharkfin/discussions@g' /usr/lib/os-release
-sed -i 's@https://github.com/ublue-os/bluefin/issues/@https://github.com/vibrantleaf/sharkfin/issues@g' /usr/lib/os-release
+sed -i 's@https://projectbluefin.io@https://github.com/vibrantleaf/LeafOS@g' /usr/lib/os-release
+sed -i 's@https://docs.projectbluefin.io@https://github.com/vibrantleaf/LeafOS/wiki@g' /usr/lib/os-release
+sed -i 's@https://discord.b@https://github.com/vibrantleaf/LeafOS/discussions@g' /usr/lib/os-release
+sed -i 's@https://github.com/ublue-os/bluefin/issues/@https://github.com/vibrantleaf/LeafOS/issues@g' /usr/lib/os-release
 sed -i 's@Bluefin@sharkfin@g' /usr/lib/os-release
 sed -i 's@bluefin@sharkfin@g' /usr/lib/os-release
 sed -i 's@universal-blue@vibrantleaf@g' /usr/lib/os-release
--- files/scripts/shetland/40-branding.sh
+++ files/scripts/shetland/40-branding.sh
@@ -3,12 +3,12 @@ set -x
 set -e
 set -u
 set -o pipefail
-sed -i 's@bluefin@borealis@g' /usr/share/ublue-os/image-info.json
+sed -i 's@ucore@shetland@g' /usr/share/ublue-os/image-info.json
 sed -i 's@ublue-os@vibrantleaf@g' /usr/share/ublue-os/image-info.json
 sed -i 's@https://projectbluefin.io@https://github.com/vibrantleaf/LeafOS@g' /usr/lib/os-release
 sed -i 's@https://docs.projectbluefin.io@https://github.com/vibrantleaf/LeafOS/wiki@g' /usr/lib/os-release
 sed -i 's@https://discord.b@https://github.com/vibrantleaf/LeafOS/discussions@g' /usr/lib/os-release
 sed -i 's@https://github.com/ublue-os/bluefin/issues/@https://github.com/vibrantleaf/LeafOS/issues@g' /usr/lib/os-release
-sed -i 's@Aurora@borealis@g' /usr/lib/os-release
-sed -i 's@aurora@borealis@g' /usr/lib/os-release
+sed -i 's@uCore@Shetland@g' /usr/lib/os-release
+sed -i 's@ucore@shetland@g' /usr/lib/os-release
 sed -i 's@universal-blue@vibrantleaf@g' /usr/lib/os-release
