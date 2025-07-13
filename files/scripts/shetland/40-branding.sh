83451c00d39d3679b7c3862a8b4152590961c32a
Julia Rees <sea.leaf@outlook.com>
2025-07-13 20:32:29 +0800

    Update 40-branding.sh
    Signed-off-by: Julia Rees <sea.leaf@outlook.com>

--- files/scripts/shetland/40-branding.sh
+++ files/scripts/shetland/40-branding.sh
@@ -3,8 +3,8 @@ set -x
 set -e
 set -u
 set -o pipefail
-sed -i 's@ucore@shetland@g' /usr/share/ublue-os/image-info.json
-sed -i 's@ublue-os@vibrantleaf@g' /usr/share/ublue-os/image-info.json
+#sed -i 's@ucore@shetland@g' /usr/share/ublue-os/image-info.json
+#sed -i 's@ublue-os@vibrantleaf@g' /usr/share/ublue-os/image-info.json
 sed -i 's@https://projectbluefin.io@https://github.com/vibrantleaf/LeafOS@g' /usr/lib/os-release
 sed -i 's@https://docs.projectbluefin.io@https://github.com/vibrantleaf/LeafOS/wiki@g' /usr/lib/os-release
 sed -i 's@https://discord.b@https://github.com/vibrantleaf/LeafOS/discussions@g' /usr/lib/os-release
