ff508aa5b051ad05c1a6e122703990e0e6d79f97
Julia Rees <sea.leaf@outlook.com>
2025-07-14 10:13:51 +0800

    add support for lossless scaling to unstable

---
+++ files/scripts/unstable/33-com.PancakeTAS.lsfg-vk.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -x
+set -e
+set -u
+set -o pipefail
+dnf5 install -y  git clang llvm cmake ninja-build spirv-headers-devel vulkan-headers vulkan-loader-devel
+git clone --recurse-submodules --depth 1 https://github.com/PancakeTAS/lsfg-vk.git  /sources/com.github.PancakeTAS.lsfg-vk
+(cd /sources/com.github.PancakeTAS.lsfg-vk;cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=Release  -DCMAKE_INSTALL_PREFIX=/usr)
+(cd /sources/com.github.PancakeTAS.lsfg-vk;cmake --build build_
+(cd /sources/com.github.PancakeTAS.lsfg-vk;cmake --install build)
+dnf5 remove -y ninja-build spirv-headers-devel vulkan-headers vulkan-loader-devel
