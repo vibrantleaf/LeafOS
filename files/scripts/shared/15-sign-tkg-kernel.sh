8e2f74990f3cdda9d0c15aca40d8fe5d3fb9908e
Julia Rees <sea.leaf@outlook.com>
2025-07-29 22:29:58 +0800

    try new method for signing tkg

--- files/scripts/shared/15-sign-tkg-kernel.sh
+++ files/scripts/shared/15-sign-tkg-kernel.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
+mkdir -p /keys"
+echo $MOK_PRIV > /keys/MOK.priv
+echo $MOK_DER > /keys/MOK.der
 KEY=/keys/MOK.priv
 CERT=/keys/MOK.der
-
 for kernel_dir in /usr/lib/modules/*_tkg*; do
   version=$(basename "$kernel_dir")
   vmlinuz_path="/usr/lib/modules/$version/vmlinuz"
-
   echo "Signing kernel: $vmlinuz_path"
-
   if [ -f "$vmlinuz_path" ]
   then
     sbsign --key "$KEY" --cert "$CERT" --output "$vmlinuz_path" "$vmlinuz_path"
@@ -23,3 +23,6 @@ for kernel_dir in /usr/lib/modules/*_tkg*; do
     echo "Skipping module signing — headers not found for $version"
   fi
 done
+mkdir -p /usr/share/leafos/secure_boot_keys/
+cp -v /keys/MOK.der /usr/share/leafos/secure_boot_keys/
+rm -rfv /keys
