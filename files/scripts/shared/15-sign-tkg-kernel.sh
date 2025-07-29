d2bb97de0c4d2fea7b810126d4dc5506c380ff8c
Julia Rees <sea.leaf@outlook.com>
2025-07-29 22:48:01 +0800

    fix build

--- files/scripts/shared/15-sign-tkg-kernel.sh
+++ files/scripts/shared/15-sign-tkg-kernel.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-mkdir -p /keys"
+mkdir -p /keys
 echo $MOK_PRIV > /keys/MOK.priv
 echo $MOK_DER > /keys/MOK.der
 KEY=/keys/MOK.priv
 CERT=/keys/MOK.der
-for kernel_dir in /usr/lib/modules/*_tkg*; do
+for kernel_dir in /usr/lib/modules/*_tkg*
+do
   version=$(basename "$kernel_dir")
   vmlinuz_path="/usr/lib/modules/$version/vmlinuz"
   echo "Signing kernel: $vmlinuz_path"
@@ -16,7 +17,8 @@ for kernel_dir in /usr/lib/modules/*_tkg*; do
   fi
   if [ -d "/usr/src/kernels/$version/scripts" ]
   then
-    find "$kernel_dir" -name '*.ko' | while read mod; do
+    find "$kernel_dir" -name '*.ko' | while read mod
+    do
       /usr/src/kernels/$version/scripts/sign-file sha256 "$KEY" "$CERT" "$mod"
     done
   else
@@ -26,3 +28,4 @@ done
 mkdir -p /usr/share/leafos/secure_boot_keys/
 cp -v /keys/MOK.der /usr/share/leafos/secure_boot_keys/
 rm -rfv /keys
+
