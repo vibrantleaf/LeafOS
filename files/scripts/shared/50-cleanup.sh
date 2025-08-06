56c896ecccc72ca73da56e6c4241ede994414a3f
Julia Rees <sea.leaf@outlook.com>
2025-08-06 19:10:45 +0800

    Update 50-cleanup.sh
    Signed-off-by: Julia Rees <sea.leaf@outlook.com>

--- files/scripts/shared/50-cleanup.sh
+++ files/scripts/shared/50-cleanup.sh
@@ -5,7 +5,7 @@ set -u
 set -o pipefail
 mkdir -p /sources
 rm -rf /sources
-rm -rf /usr/share/gnome-shell/extensions/tilingshell@ferrarodomenico.com
+#rm -rf /usr/share/gnome-shell/extensions/tilingshell@ferrarodomenico.com
 rm -rf /usr/share/sounds/steam
 rm -f /usr/etc/.gitkeep
 rm -f /usr/sbin/protontricks
