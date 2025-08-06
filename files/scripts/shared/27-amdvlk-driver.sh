fd21b357cac43b97ab9c58e17e64f7b54992cd5f
Julia Rees <sea.leaf@outlook.com>
2025-08-06 18:53:14 +0800

    Update 27-amdvlk-driver.sh
    Signed-off-by: Julia Rees <sea.leaf@outlook.com>

--- files/scripts/shared/27-amdvlk-driver.sh
+++ files/scripts/shared/27-amdvlk-driver.sh
@@ -1,2 +1,3 @@
 #!/use/bin/env bash
-dnf install -y $(curl -s https://api.github.com/repos/GPUOpen-Drivers/AMDVLK/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep .x86_64.rpm)
+curl -L -O /sources/amdvlk-latest.rpm $(curl -s https://api.github.com/repos/GPUOpen-Drivers/AMDVLK/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep .x86_64.rpm)
+dnf install -y /sources/amdvlk-latest.rpm
