2b7cb31c285255ae1cd3d0616072e730d14abfb6
Julia Rees <sea.leaf@outlook.com>
2025-09-17 08:59:22 +0800

    dropped: AMDVLK driver as it's discontuned https://github.com/GPUOpen-Drivers/AMDVLK/discussions/416

--- files/scripts/shared/27-amdvlk-driver.sh
+++
@@ -1,3 +0,0 @@
-#!/use/bin/env bash
-curl -L -O /sources/amdvlk-latest.rpm $(curl -s https://api.github.com/repos/GPUOpen-Drivers/AMDVLK/releases/latest | grep browser_download_url | cut -d '"' -f 4 | grep .x86_64.rpm)
-dnf install -y /sources/amdvlk-latest.rpm
