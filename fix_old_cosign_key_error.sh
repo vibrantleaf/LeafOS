880104a3452919c4dc38a600d1a03cc93d0fdb78
Julia Rees <sea.leaf@outlook.com>
2025-09-22 10:21:12 +0800

    Update fix_old_cosign_key_error.sh
    Signed-off-by: Julia Rees <sea.leaf@outlook.com>

--- fix_old_cosign_key_error.sh
+++ fix_old_cosign_key_error.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Downloading New Cosing Key"
 curl https://raw.githubusercontent.com/vibrantleaf/LeafOS/refs/heads/main/cosign.pub | sudo tee /etc/pki/containers/{sharkfin,borealis,sharkbox,shetland}.pub > /dev/null
+curl https://raw.githubusercontent.com/ublue-os/main/refs/heads/main/cosign.pub | sudo tee /etc/pki/containers/ublue-os.pub
 echo "Upgrading Bootc Image"
 sudo bootc upgrade
